057dbaa1b99c94fef689be96970c3ff263e06a3c
Kyrylo Meretskyi <kyrylo.meretskyi@spryker.com>
2022-05-03 19:57:22 +0300

    APPS-3757: Fixed Spryks and adjust tests

---
+++ tests/SprykerSdkTest/Spryk/Integration/Glue/BackendApi/Mapper/AddGlueBackendApiResponseMapperTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace SprykerSdkTest\Spryk\Integration\Glue\BackendApi\Mapper;
+
+use Codeception\Test\Unit;
+use SprykerSdkTest\Module\GlueBackendApiClassNames;
+
+/**
+ * Auto-generated group annotations
+ *
+ * @group SprykerSdkTest
+ * @group Spryk
+ * @group Integration
+ * @group Glue
+ * @group BackendApi
+ * @group Mapper
+ * @group AddGlueBackendApiResponseMapperTest
+ * Add your own group annotations below this line
+ */
+class AddGlueBackendApiResponseMapperTest extends Unit
+{
+    /**
+     * @var string
+     */
+    protected const CLASS_NAME = '\Spryker\Glue\FooBar\ResponseMapper\GlueResponseFooBarResponseMapper';
+
+    /**
+     * @var string
+     */
+    protected const MODULE = 'FooBar';
+
+    /**
+     * @var \SprykerSdkTest\SprykIntegrationTester
+     */
+    protected $tester;
+
+    /**
+     * @return void
+     */
+    public function testAddsGlueBackendApiResponseMapper(): void
+    {
+        $this->tester->run($this, [
+            '--resource' => '/foo-bars',
+            '--module' => static::MODULE,
+        ]);
+
+        $this->tester->assertClassOrInterfaceExists(static::CLASS_NAME);
+        $this->tester->assertClassOrInterfaceExists(static::CLASS_NAME.'Interface');
+
+        $this->tester->assertClassHasMethod(static::CLASS_NAME, 'map' . static::MODULE . 'CollectionTransferToGlueResponseTransfer');
+        $this->tester->assertClassHasMethod(static::CLASS_NAME, 'map' . static::MODULE . 'CollectionTransferToSingleResourceGlueResponseTransfer');
+        $this->tester->assertClassHasMethod(static::CLASS_NAME, 'map' . static::MODULE . 'CollectionResponseTransferToGlueResponseTransfer');
+        $this->tester->assertClassHasMethod(static::CLASS_NAME, 'map' . static::MODULE . 'CollectionResponseTransferToSingleResourceGlueResponseTransfer');
+        $this->tester->assertClassHasMethod(static::CLASS_NAME, 'addNotFoundError');
+        $this->tester->assertClassHasMethod(static::CLASS_NAME, 'addResourceToGlueResponse');
+    }
+
+}
