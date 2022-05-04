d787fd7f9ca856106afbcc90896d3f490aa48c11
mikhail <kopylets@logema.org>
2022-05-04 19:06:33 +0300

    [APPS-3778-APPS-3782] Create Resource Plugin and Create ResourcePlugin update Spryk

---
+++ src/Spryk/Model/Spryk/Filter/CamelCaseToDashFilter.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace SprykerSdk\Spryk\Model\Spryk\Filter;
+
+use Laminas\Filter\FilterChain;
+use Laminas\Filter\StringToLower;
+use Laminas\Filter\Word\CamelCaseToDash;
+use Laminas\Filter\Word\DashToCamelCase;
+
+/**
+ * Filter is used to convert a camelCased string
+ * into string where words are separated by "-".
+ *
+ * Example:
+ * $this->filter('stringExample') === `string-example';
+ */
+class CamelCaseToDashFilter implements FilterInterface
+{
+    /**
+     * @var string
+     */
+    protected const FILTER_NAME = 'camelCaseToDash';
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return static::FILTER_NAME;
+    }
+
+    /**
+     * @param string $value
+     *
+     * @return string
+     */
+    public function filter(string $value): string
+    {
+        $filterChain = new FilterChain();
+        $filterChain
+            ->attach(new CamelCaseToDash())
+            ->attach(new StringToLower());
+
+        return $filterChain->filter($value);
+    }
+}
