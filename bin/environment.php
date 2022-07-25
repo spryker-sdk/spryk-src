39d4d7209baf390607c5089a55014d51f802cd70
Olha Livitchuk <olha.livitchuk@spryker.com>
2022-07-25 18:25:28 +0300

    FRW-112 refactored the const name

--- bin/environment.php
+++ bin/environment.php
@@ -57,5 +57,5 @@ $allowedApplicationEnvs = [
 ];
 
 defined('APPLICATION_ROOT_DIR') || define('APPLICATION_ROOT_DIR', getcwd());
-define('APPLICATION_ENV_SPRYK_USAGE', $applicationEnv !== false && in_array($applicationEnv, $allowedApplicationEnvs, true) ? $applicationEnv : 'prod');
-define('APPLICATION_DEBUG', getenv('APPLICATION_DEBUG') !== false ? (bool)getenv('APPLICATION_DEBUG') : APPLICATION_ENV_SPRYK_USAGE !== 'prod');
+define('APPLICATION_ENV_SPRYK', $applicationEnv !== false && in_array($applicationEnv, $allowedApplicationEnvs, true) ? $applicationEnv : 'prod');
+define('APPLICATION_DEBUG', getenv('APPLICATION_DEBUG') !== false ? (bool)getenv('APPLICATION_DEBUG') : APPLICATION_ENV_SPRYK !== 'prod');
