19a2562b96402279ca1fe558e2beb8b7bd0e4ee7
dima_tsemma <dmitry.tsemma@spryker.com>
2022-12-01 19:40:38 +0200

    FRW-92 Refactored after AA review

--- tests/_support/Module/GlueStorefrontApiClassNames.php
+++ tests/_support/Module/GlueStorefrontApiClassNames.php
@@ -12,47 +12,7 @@ interface GlueStorefrontApiClassNames
     /**
      * @var string
      */
-    public const PROJECT_GLUE_STOREFRONT_API_BOOTSTRAP = 'Pyz\Glue\GlueApplication\Bootstrap\GlueStorefrontApiBootstrap';
-
-    /**
-     * @var string
-     */
-    public const GLUE_STOREFRONT_API_BOOTSTRAP = 'Spryker\Glue\GlueApplication\Bootstrap\GlueStorefrontApiBootstrap';
-
-    /**
-     * @var string
-     */
-    public const GLUE_APPLICATION_DEPENDENCY_PROVIDER = 'Spryker\Glue\GlueApplication\GlueApplicationDependencyProvider';
-
-    /**
-     * @var string
-     */
-    public const PROJECT_GLUE_APPLICATION_DEPENDENCY_PROVIDER = 'Pyz\Glue\GlueApplication\GlueApplicationDependencyProvider';
-
-    /**
-     * @var string
-     */
-    public const GLUE_REST_API_CONVENTION_DEPENDENCY_PROVIDER = 'Spryker\Glue\GlueRestApiConvention\GlueRestApiConventionDependencyProvider';
-
-    /**
-     * @var string
-     */
-    public const GLUE_STOREFRONT_API_APPLICATION_DEPENDENCY_PROVIDER = 'Spryker\Glue\GlueStorefrontApiApplication\GlueStorefrontApiApplicationDependencyProvider';
-
-    /**
-     * @var string
-     */
     public const PROJECT_GLUE_STOREFRONT_API_APPLICATION_DEPENDENCY_PROVIDER = 'Pyz\Glue\GlueStorefrontApiApplication\GlueStorefrontApiApplicationDependencyProvider';
-
-    /**
-     * @var string
-     */
-    public const GLUE_STOREFRONT_API_FACTORY = 'Spryker\Glue\FooBarsStorefrontApi\FooBarsStorefrontApiFactory';
-
-    /**
-     * @var string
-     */
-    public const GLUE_STOREFRONT_API_DEPENDENCY_PROVIDER = 'Spryker\Glue\FooBarsStorefrontApi\FooBarsStorefrontApiDependencyProvider';
 
     /**
      * @var string
@@ -63,46 +23,6 @@ interface GlueStorefrontApiClassNames
      * @var string
      */
     public const GLUE_STOREFRONT_API_INDEX_CONTROLLER_TEST = 'SprykerTest\Glue\FooBarsStorefrontApi\Controller\IndexResourceControllerTest';
-
-    /**
-     * @var string
-     */
-    public const GLUE_STOREFRONT_API_CONTROLLER = 'Spryker\Glue\FooBarsStorefrontApi\Controller\FooBarsResourceController';
-
-    /**
-     * @var string
-     */
-    public const GLUE_BACKEND_API_CONTROLLER_TEST = 'SprykerTest\Glue\FooBarsStorefrontApi\Controller\FooBarsResourceControllerTest';
-
-    /**
-     * @var string
-     */
-    public const GLUE_STOREFRONT_API_GET_CONTROLLER_TEST = 'SprykerTest\Glue\FooBarsStorefrontApi\RestApi\FooBarsGetRestApiCest';
-
-    /**
-     * @var string
-     */
-    public const GLUE_STOREFRONT_API_GET_COLLECTION_CONTROLLER_TEST = 'SprykerTest\Glue\FooBarsStorefrontApi\Controller\FooBarsGetCollectionResourceControllerTest';
-
-    /**
-     * @var string
-     */
-    public const GLUE_STOREFRONT_API_POST_CONTROLLER_TEST = 'SprykerTest\Glue\FooBarsStorefrontApi\Controller\FooBarsPostResourceControllerTest';
-
-    /**
-     * @var string
-     */
-    public const GLUE_STOREFRONT_API_PATCH_CONTROLLER_TEST = 'SprykerTest\Glue\FooBarsStorefrontApi\Controller\FooBarsPatchResourceControllerTest';
-
-    /**
-     * @var string
-     */
-    public const GLUE_STOREFRONT_API_DELETE_CONTROLLER_TEST = 'SprykerTest\Glue\FooBarsStorefrontApi\Controller\FooBarsDeleteResourceControllerTest';
-
-    /**
-     * @var string
-     */
-    public const GLUE_STOREFRONT_API_DELETE_COLLECTION_CONTROLLER_TEST = 'SprykerTest\Glue\FooBarsStorefrontApi\Controller\FooBarsDeleteCollectionResourceControllerTest';
 
     /**
      * @var string
@@ -128,9 +48,4 @@ interface GlueStorefrontApiClassNames
      * @var string
      */
     public const GLUE_STOREFRONT_API_RESPONSE_MAPPER_INTERFACE = '\Spryker\Glue\FooBarsStorefrontApi\Mapper\GlueResponseFooBarMapperInterface';
-
-    /**
-     * @var string
-     */
-    public const GLUE_TEST_HELPER = 'SprykerTest\Glue\FooBarsStorefrontApi\Helper\FooBarsHelper';
 }
